d61116f2e77e331f67560ae47f0c55fd7a402d89
silentBit0 <shavnalla@gmail.com>
2026-05-15 01:07:01 +0530

    Add files via upload

--- expt3_first_follow.py
+++ expt3_first_follow.py
@@ -1,89 +1,86 @@
 grammar = {}
 n = int(input("Enter number of productions: "))
-print("Enter productions")
+print("Enter productions:")
 
 for i in range(n):
-  rule = input()
-  left, right = rule.split('->')
-  grammar[left] = right.split('|')
+    rule = input()
+    left, right = rule.split("->")
+    grammar[left] = right.split("|")
 
 def tokenize(prod):
-  tokens = []
-  i = 0
-  while i < len(prod):
-    if i + 1 < len(prod) and prod[i + 1] == "'":
-      tokens.append(prod[i] + "'")
-      i += 2
-    else:
-      tokens.append(prod[i])
-      i += 1
-  return tokens
+    tokens = []
+    i = 0
+    while i < len(prod):
+        if i + 1 < len(prod) and prod[i + 1] == "'":
+            tokens.append(prod[i] + "'")
+            i += 2
+        else:
+            tokens.append(prod[i])
+            i += 1
+    return tokens
 
 new_grammar = {}
 
 for nt in grammar:
-  alpha = []
-  beta = []
-  for prod in grammar[nt]:
-    tokens = tokenize(prod)
-    if tokens[0] == nt:
-      alpha.append(tokens[1:])
+    alpha = []
+    beta = []
+    for prod in grammar[nt]:
+        tokens = tokenize(prod)
+        if tokens[0] == nt:
+            alpha.append(tokens[1:])
+        else:
+            beta.append(tokens)
+    if alpha:
+        new_nt = nt + "'"
+        new_grammar[nt] = []
+        for b in beta:
+            new_grammar[nt].append(b + [new_nt])
+        new_grammar[new_nt] = []
+        for a in alpha:
+            new_grammar[new_nt].append(a + [new_nt])
+        new_grammar[new_nt].append(["e"])
     else:
-      beta.append(tokens)
-  if alpha:
-    new_nt = nt + "'"
-    new_grammar[nt] = []
-    for b in beta:
-      new_grammar[nt].append(b + [new_nt])
-    new_grammar[new_nt] = []
-    for a in alpha:
-      new_grammar[new_nt].append(a + [new_nt])
-    new_grammar[new_nt].append(['e'])
-  else:
-    new_grammar[nt] = [tokenize(p) for p in grammar[nt]]
+        new_grammar[nt] = [tokenize(p) for p in grammar[nt]]
 
-print("\nGrammar after removing left recursion:\n")
+print("\nGrammar after removing left recursion:")
 for nt in new_grammar:
-  rhs = []
-  for prod in new_grammar[nt]:
-    rhs.append("".join(prod))
-  print(nt, '->', ' | '.join(rhs))
-
+    rhs = []
+    for prod in new_grammar[nt]:
+        rhs.append("".join(prod))
+    print(nt, "->", " | ".join(rhs))
 
 first = {}
 for nt in new_grammar:
-  first[nt] = set()
-  for prod in new_grammar[nt]:
-    first[nt].add(prod[0])
+    first[nt] = set()
+    for prod in new_grammar[nt]:
+        first[nt].add(prod[0])
 
-
-print('\nFIRST\n')
+print("\nFIRST:")
 for nt in first:
-  print(nt, ": ", first[nt])
+    print(nt, ": ", first[nt])
 
 follow = {}
 
 for nt in new_grammar:
-  follow[nt] = set()
+    follow[nt] = set()
 
 start = list(new_grammar.keys())[0]
-follow[start].add('$')
-
+follow[start].add("$")
 
 for head in new_grammar:
-  for prod in new_grammar[head]:
-    for i in range(len(prod)):
-      symbol = prod[i]
-      if symbol in new_grammar:
-        if i + 1 < len(prod):
-          next_symbol = prod[i + 1]
-          if next_symbol in first:
-            follow[symbol].update(first[next_symbol] - {'e'})
-          else:
-            follow[symbol].add(next_symbol)
-        else:
-          follow[symbol].update(follow[head])
+    for prod in new_grammar[head]:
+        for i in range(len(prod)):
+            symbol = prod[i]
+            if symbol in new_grammar:
+                if i + 1 < len(prod):
+                    next_symbol = prod[i + 1]
+                    if next_symbol in first:
+                        follow[symbol].update(first[next_symbol] - {"e"})
+                    else:
+                        follow[symbol].add(next_symbol)
+                else:
+                    follow[symbol].update(follow[head])
 
-print('\nFOLLOW\n')
+print("\nFOLLOW:")
 for nt in follow:
-  print(nt, ": ", follow[nt])
+    print(nt, ": ", follow[nt])
--- expt4_ll1.py
+++ expt4_ll1.py
@@ -1,71 +1,71 @@
 table = {
-  ('E', 'i') : 'TA',
-  ('E', '(') : 'TA', 
+    ("E", "i"): "TA",
+    ("E", "("): "TA",
 
-  ('A', '+') : '+TA',
-  ('A', ')') : 'e',
-  ('A', '$') : 'e',
- 
-  ('T', 'i') : 'FB',
-  ('T', '(') : 'FB',
- 
-  ('B', '*') : '*FB',
-  ('B', '+') : 'e',
-  ('B', ')') : 'e',
-  ('B', '$') : 'e',
- 
-  ('F', 'i') : 'i',
-  ('F', '(') : '(E)'
+    ("A", "+"): "+TA",
+    ("A", ")"): "e",
+    ("A", "$"): "e",
+
+    ("T", "i"): "FB",
+    ("T", "("): "FB",
+
+    ("B", "*"): "*FB",
+    ("B", "+"): "e",
+    ("B", ")"): "e",
+    ("B", "$"): "e",
+
+    ("F", "i"): "i",
+    ("F", "("): "(E)",
 }
 
-def isNonTerminal(ch):
-  return ch.isupper()
+def is_non_terminal(ch):
+    return ch.isupper()
 
-print('Grammar used: ')
-print('E->TA')
-print('A->+TA|e')
-print('T->FB')
-print('B->*FB|e')
-print('B->(E)|i')
+print("Grammar used: ")
+print("E->TA")
+print("A->+TA|e")
+print("T->FB")
+print("B->*FB|e")
+print("B->(E)|i")
 
-input_str = input("Enter input string: ")
-if input_str[-1] != '$':
-  input_str += '$'
+input_str = input("\nEnter input string: ")
+if input_str[-1] != "$":
+    input_str += "$"
 
 stack = []
-stack.append('$')
-stack.append('E')
+stack.append("$")
+stack.append("E")
 pointer = 0
 
-print('STACK\t\tINPUT\t\tACTION')
+print(f"\n{'STACK':<20}{'INPUT':<20}{'ACTION'}")
 
 while len(stack) > 0:
-  top = stack[-1]
-  curr = input_str[pointer]
-  stack_str = "".join(stack)
-  remaining_input = input_str[pointer:]
-  if top == curr:
-    print(stack_str, '\t\t', remaining_input, '\t\t Match', curr)
-    stack.pop()
-    pointer += 1
-    
-    if top == '$':
-      print('\nSTRING ACCEPTED')
-      break
-  elif isNonTerminal(top):
-    key = (top, curr)
-    if key in table:
-      production = table[key]
-      print(stack_str,'\t\t', remaining_input, '\t\t', top, '->', production)
-      stack.pop()
-      if production != 'e':
-        for symbol in reversed(production):
-          stack.append(symbol)
+    top = stack[-1]
+    curr = input_str[pointer]
+    stack_str = "".join(stack)
+    remaining_input = input_str[pointer:]
+    if top == curr:
+        print(f"{stack_str:<20}{remaining_input:<20}Match {curr}")
+        stack.pop()
+        pointer += 1
+
+        if top == "$":
+            print("\nSTRING ACCEPTED")
+            break
+    elif is_non_terminal(top):
+        key = (top, curr)
+        if key in table:
+            production = table[key]
+            print(f"{stack_str:<20}{remaining_input:<20}{top} -> {production}")
+            stack.pop()
+            if production != "e":
+                for symbol in reversed(production):
+                    stack.append(symbol)
+        else:
+            print(f"{stack_str:<20}{remaining_input:<20}ERROR")
+            print("STRING REJECTED")
+            break
     else:
-      print(stack_str, '\t\t', remaining_input, '\t\terror')
-      print('STRING REJECTED')
-      break
-  else:
-    print(stack_str, '\t\t', remaining_input, '\t\terror')
-    print('STRING REJECTED')
-    break
+        print(f"{stack_str:<20}{remaining_input:<20}ERROR")
+        print("STRING REJECTED")
+        break
